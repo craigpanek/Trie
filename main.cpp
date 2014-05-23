c419707ce78edae2e04b6b61745b540d7b7bc52a
craigpanek <panekcraig@yahoo.com>
2014-05-22 22:00:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,22 +18,6 @@ int main() {
         cout << word << " is" << (lexicon.isWord(word) ? "":" NOT") << " a word\n";
         cin >> word;
     }
-/*
-    Trie trie;
-
-	trie.insert("apple");
-	trie.insert("apply");
-	trie.insert("orange");
-	trie.insert("or");
-
-	cout << "apple is a word: " << trie.exists("apple") << endl;
-	cout << "apply is a word: " << trie.exists("apply") << endl;
-	cout << "orange is a word: " << trie.exists("orange") << endl;
-	cout << "oranges is a word: " << trie.exists("oranges") << endl;
-	cout << "or is a word: " << trie.exists("orange") << endl;
-	cout << "app is a word: " << trie.exists("app") << endl;
-
 	//system("pause"); // if needed, comment out but don't delete
-*/
 	return 0;
 }
